000291aa3faa8d50cbf01842f0a019d82c3cc5b5
paulbelches <paulbelches@gmail.com>
2020-08-25 18:41:53 -0600

    Declaration fix

--- functions/workdir/PerWordAnalisys.py
+++ functions/workdir/PerWordAnalisys.py
@@ -2,15 +2,20 @@ import nltk
 nltk.download('vader_lexicon')
 
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-test_subset="Lots of fun in the bath but have to use a lot to make it bubble! Nice addition when the kids don't"
+
 #print(test_subset.split())
 def perWordAnalisys(text):
     words = text.split()
     sid = SentimentIntensityAnalyzer()
-    pos, neg = 0
+    pos, neg = 0, 0
     for word in words:
         if (sid.polarity_scores(word)['compound']) >= 0.5:
             pos+= 1
         elif (sid.polarity_scores(word)['compound']) <= -0.5:
             neg+= 1        
     return pos - neg  
+
+
+#test_subset="Lots of fun in the bath but have to use a lot to make it bubble! Nice addition when the kids don't"
+test_subset="Good flavor. This review was collected as part of a promotion."
+print(perWordAnalisys(test_subset))
